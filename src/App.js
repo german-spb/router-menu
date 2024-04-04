302d201f17883d3bf86994ef1063c6a4d09ead81
Корецкий Герман Анатольевич <gkoretskiy@sbk.ru>
2024-04-04 15:25:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu/Menu';
+import HomePage from './components/HomePage/HomePage';
+import DriftPage from './components/DriftPage/DriftPage';
+import ForzaPage from './components/ForzaPage/ForzaPage';
+import TimeAttackPage from './components/TimeAttackPage/TimeAttackPage';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
